a853a4f0d33758ebc7dcadeb1923d0ee626054a8
Levi Hutchins <levihutchins0@gmail.com>
2024-08-03 20:33:36 +1000

    event handlers for inputbox

--- src/Components/InputBox/InputBox.jsx
+++ src/Components/InputBox/InputBox.jsx
@@ -4,24 +4,31 @@ import TextField from "@mui/material/TextField";
 import "./InputBox.css";
 
 const InputBox = (props) => {
-  const [count, setCount] = useState(0);
-
+  const [inputValue, setInputValue] = useState("");
+  const handleSubmit = () => {
+    alert("Submittedddddddd");
+  };
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+  };
   return (
     <>
-      <Box
-        component="form"
-        className="input-box"
-        noValidate
-        autoComplete="off"
-      >
-        <TextField
-          label= {props.displayValue}
-          variant="outlined"
-          className="input-field"
-        />
-              <TextField id="filled-basic" label="Filled" variant="filled" />
-
-      </Box>
+      <form onSubmit={handleSubmit}>
+        <Box
+          component="form"
+          className="input-box"
+          noValidate
+          autoComplete="off"
+        >
+          <TextField
+            label={props.displayValue}
+            variant="outlined"
+            className="input-field"
+            sx={{ input: { color: "rgb(185, 185, 185)" } }}
+            onChange={handleChange}
+          />
+        </Box>
+      </form>
     </>
   );
 };
