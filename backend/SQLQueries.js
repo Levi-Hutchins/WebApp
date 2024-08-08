841c28f3fca79162e4e641d53c7855d4ad0d3b9c
Levi Hutchins <levihutchins0@gmail.com>
2024-08-08 21:09:28 +1000

    simple backend server to query the sql instance for development; dyamic table template from MUI

---
+++ backend/SQLQueries.js
@@ -0,0 +1,38 @@
+
+const sql = require('mssql');
+const databaseConfig = require('./SQLConfig'); // Assuming you have a SQLConfig.js file exporting database configuration
+
+const databaseConfig = {
+    user: process.env.USERACC,  
+    password: process.env.PASSWORD, 
+    server: process.env.SERVER,    
+    port: parseInt(process.env.PORT,10),           
+    database: process.env.DATABASE, 
+
+    options: {
+        encrypt: true,       
+        trustServerCertificate: true 
+    }}
+
+async function searchProducts(productName) {
+
+    try{
+        let db_connection = await sql.connect(databaseConfig);
+
+        let result = await db_connection.request()
+        .input('productName', sql.VarChar, productName)
+        .query('SELECT TOP 10 * FROM [master].[dbo].[Product] WHERE Name = @productName');
+
+        
+        await sql.close();
+
+        
+        return result.recordset
+
+    } catch(error){
+        console.error(error);
+        return null
+    }
+}
+
+module.exports = { searchProducts };
--- src/Pages/SearchPage/SearchPage.jsx
+++ src/Pages/SearchPage/SearchPage.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import axios from 'axios';
+
 import { useState } from "react";
 import "@fontsource/archivo-black";
 import "./SearchPage.css";
-import "../../Components/InputBoxWithButton/InputBoxWithButton.css";
 
 import InputBoxWithButton from "../../Components/InputBoxWithButton/InputBoxWithButton";
+import "../../Components/InputBoxWithButton/InputBoxWithButton.css";
 
 const SearchPage = () => {
+  const [productSubmitted, setProductSubmitted] = useState(false);
   const [inputValue, setInputValue] = useState("");
 
   const handleChange = (event) => {
@@ -14,24 +17,41 @@ const SearchPage = () => {
     setInputValue(event.target.value);
   };
 
-  const handleSearch = () => {
-    alert(inputValue);
+  const handleSubmit = () => {
+    setProductSubmitted(true);
+    axios.post("http://localhost:4000/api/search", { product: inputValue })
+    .then(res => {
+      console.log(res);
+      console.log(res.data);
+    })
+    .catch(err => {
+      console.error(err);
+    });
+
+
+
+
+
   };
 
   return (
     <div>
       <div className="search-title">
-        <h1 style={{ color: "#5e43f3" }}>FIND</h1>
-        <h1 style={{ color: "white" }}> AN ITEM</h1>
+        <h1 className="title-primary">FIND</h1>
+        <h1 className="title-secondary"> AN ITEM</h1>
       </div>
 
       <div className="components">
-        <InputBoxWithButton
-          displayValue="Search For an Item"
-          onChange={handleChange}
-          onSubmit={handleSearch}
-          inputValueProps={inputValue}
-        />
+        {productSubmitted ? (
+          <h1>TRUE</h1>
+        ) : (
+          <InputBoxWithButton
+            displayValue="Search For an Item"
+            onChange={handleChange}
+            onSubmit={handleSubmit}
+            inputValueProps={inputValue}
+          />
+        )}
       </div>
     </div>
   );
