8b938f32ccbe71ae0d7b879616ec7d8b726210b5
Levi Hutchins <levihutchins0@gmail.com>
2024-08-10 09:56:38 +1000

    handleSubmit implemented to query db with the inputted product

--- src/Pages/SearchPage/SearchPage.jsx
+++ src/Pages/SearchPage/SearchPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import axios from 'axios';
+import axios from "axios";
 
 import { useState } from "react";
 import "@fontsource/archivo-black";
@@ -8,9 +8,12 @@ import "./SearchPage.css";
 import InputBoxWithButton from "../../Components/InputBoxWithButton/InputBoxWithButton";
 import "../../Components/InputBoxWithButton/InputBoxWithButton.css";
 
+//TODO: Have a look at MUI autocomplete component with a hook on page load that queries
+// the database for all the products to be loaded in the autocomplte 
+
 const SearchPage = () => {
   const [productSubmitted, setProductSubmitted] = useState(false);
-  const [productsFound, setProductsFound] = useState('');
+  const [productsFound, setProductsFound] = useState("");
 
   const [inputValue, setInputValue] = useState("");
 
@@ -21,19 +24,15 @@ const SearchPage = () => {
 
   const handleSubmit = () => {
     setProductSubmitted(true);
-    axios.post("http://localhost:4000/api/search", { product: inputValue })
-    .then(res => {
-      set
-      console.log(res.data);
-    })
-    .catch(err => {
-      console.error(err);
-    });
-
-
-
-
-
+    axios
+      .post("http://localhost:4000/api/search", { product: inputValue })
+      .then((res) => {
+        set;
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
   };
 
   return (
