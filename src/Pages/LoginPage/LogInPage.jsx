331193694395dec09f9c1b7a9f62c2c49110a7ad
Corey Silk <coreysilk14@hotmail.com>
2024-09-16 14:12:29 +1000

    Merged branch Login-Page-V2 into main

---
+++ src/Pages/LoginPage/LogInPage.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { useRef, useState, useEffect } from "react"
+import Login from '../../Components/Login/LoginPortal';
+
+const LogInPage = () => {
+
+  const userRef = useRef();
+  const errRef = useRef(); /*used for error handling and screen reading for accessibility*/ 
+
+  // states for username
+  const [user, setUser] = useState('');
+  const [pword, setPword] = useState('');
+  // state for error messaging and success
+  const [errMsg, setErrMsg] = useState('');
+  const [success, setSuccess] = useState(false);
+
+  useEffect(() => {
+    userRef.current.focus();
+  }, [])
+
+  useEffect(() => {
+    setErrMsg('');
+  }, [user, pword])
+
+
+
+  return (
+    <div>
+      <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live='assertive'>{errMsg}</p>
+      <h1>Log In</h1>
+      <Login
+      user={user}
+      setUser = {setUser}
+      pword={pword}
+      setPword = {setPword}
+      userRef = {userRef}      
+      />
+    </div>
+  )
+}
+
+export default LogInPage;
