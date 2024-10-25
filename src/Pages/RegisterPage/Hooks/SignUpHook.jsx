a5dcfd3c93e496936108fb071fadeae751e11419
Levi Hutchins <levihutchins0@gmail.com>
2024-10-25 13:27:35 +1100

    register page fixed

--- src/Pages/RegisterPage/Hooks/SignUpHook.jsx
+++ src/Pages/RegisterPage/Hooks/SignUpHook.jsx
@@ -1,79 +1,71 @@
 import { useState } from "react";
 import axios from "axios";
-import {hashPassword} from "../../../Utils/HahingService"
+import { toast } from "react-toastify";
+import { hashPassword } from "../../../Utils/HahingService";
+import useValidation from "./useValidation";
 
-const useForm = (initialValues, signUpValidator, toast) => {
+const useForm = (initialValues) => {
   const [values, setValues] = useState(initialValues);
-  const [errors, setErrors] = useState({});
-
+  const { validateSignUp } = useValidation();
   const handleChange = (e) => {
-
     const { name, value } = e.target;
 
     setValues({
       ...values,
       [name]: value,
     });
+  }
 
-    const fieldError = signUpValidator({ ...values, [name]: value })[name];
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      if (!validateSignUp(values)) {
+        return;
+      }
 
-    setErrors((prevErrors) => ({
-      ...prevErrors,
-      [name]: fieldError,
-    }));
-    console.log(values)
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    const validationErrors = signUpValidator(values);
-    setErrors(validationErrors);
-
-    if (Object.keys(validationErrors).length === 0) {
       try {
         await axios
           .get(
-            "http://localhost:8080/api/v1/db/data/v1/inft3050/User/find-one",
+            "http://localhost:8080/api/v1/db/data/v1/inft3050/Patrons/find-one",
             {
               headers: {
                 "xc-token": process.env.REACT_APP_APIKEY,
               },
               params: {
-                where: `(Email,eq,${values.emailAddress})`,
+                where: `(Email,eq,${values.Email})`,
               },
             }
           )
           .then((user) => {
             if (!Object.keys(user.data).length) {
-
-              const passwordSalt = hashPassword(values.password)
+              const passwordSalt = hashPassword(values.password);
               const userToAdd = {
-                UserName: values.userName,
-                Email: values.emailAddress,
-                Name: values.fullName,
-                // TODO: Fix this does seem to be getting the right value
-                isAdmin: values.isAdmin ? "true": "false",
+                Email: values.Email,
+                Name: values.Name,
                 Salt: passwordSalt.salt,
                 HashPW: passwordSalt.hash,
-              }
-              axios.post("http://localhost:8080/api/v1/db/data/v1/inft3050/User", userToAdd, {
-                headers:{
-                  "xc-token": process.env.REACT_APP_APIKEY,
-                }
-              }).then((user) => {
-                if (user){
-                  toast.success("User Created !", {
+              };
+              axios
+                .post(
+                  "http://localhost:8080/api/v1/db/data/v1/inft3050/Patrons",
+                  userToAdd,
+                  {
+                    headers: {
+                      "xc-token": process.env.REACT_APP_APIKEY,
+                    },
+                  }
+                )
+                .then((user) => {
+                  if (user) {
+                    toast.success("User Created !", {
+                      position: "bottom-right",
+                    });
+                  }
+                })
+                .catch((error) => {
+                  toast.error("Oops ! An error occurred", {
                     position: "bottom-right",
                   });
-                }
-              }).catch((error) => {
-                toast.error("Oops ! An error occurred",{
-                  position: "bottom-right",
-                })
-              })
-         
-             
+                });
             } else {
               toast.error("User already exists with this email", {
                 position: "bottom-right",
@@ -89,20 +81,15 @@ const useForm = (initialValues, signUpValidator, toast) => {
           }
         );
       }
-    } else {
-      toast.error("Please correct highlighted fields", {
-        position: "bottom-right",
-      });
-    }
-  };
+    };
 
-  return {
-    values,
-    errors,
-    handleChange,
-    handleSubmit,
-    setValues
-  };
+    return {
+      values,
+      handleChange,
+      handleSubmit,
+      setValues,
+    };
+  
 };
 
 export default useForm;
--- src/Pages/RegisterPage/RegisterPage.jsx
+++ src/Pages/RegisterPage/RegisterPage.jsx
@@ -18,9 +18,9 @@ const RegisterPage = () => {
   return (
     <div>
       <div className={styles["signup-title"]}>
-        <h1 className={styles["signup-title-primary"]}>CREATE</h1>
-        <h1 className={styles["signup-title-secondary"]}> A </h1>
-        <h1 className={styles["signup-title-secondary"]}>USER !</h1>
+        <h1 className={styles["signup-title-primary"]}>Register</h1>
+        <h1 className={styles["signup-title-secondary"]}> Now </h1>
+        <h1 className={styles["signup-title-secondary"]}>!</h1>
         <h2 className={styles["sub-title"]}>Already have an account ? {<Link className={styles["login-link"]} to={'/Login'}>Log In </Link>}</h2>
       </div>
       <div className={styles["div-border"]}>
